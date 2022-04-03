8ec97e4be9c36d5ff439bc6b8b501ebb5131784c
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-03 13:30:39 +0100

    Première version fonctionnel du traducteur URL query => SQL query

--- api-cegid/util/parametres.js
+++ api-cegid/util/parametres.js
@@ -12,7 +12,7 @@ class Query {
         }
         this.allowedFields = allowedFields;
         this.queryString="";
-        this.inputs=[];
+        this.inputs={};
     }
 
     _parse(queryString) {
@@ -35,21 +35,29 @@ class Query {
                 return `Erreur : le field ${field} n'est pas autorisé`;
             }
             const fields = this.queryString[field]
+            
             let i=0;
+            //Le i est nécessaire pour protéger les inputs contre les attaques SQL
+            //le i sert de nom aux inputs dans le cas ou le même attribut a plusieurs condition
+            //Exemple : b>@b AND b<@b ne fonctionnerait pas. Mais b>@1 AND b<@2 (les chiffres sont la valeur de i)fonctionnee
+            
             for (const param of Object.keys(fields)) {
                 i++;
                 if (param in this.operators) {
+                    //TODO: Remplacer la vraie value par le @ du prepared statement
                     result+=`${field} ${this.operators[param]} ${fields[param]}` 
-                    this.inputs.push(field)
+                    this.inputs[i]= fields[param];
                 }
                 else {
                     if (this.queryString[field].constructor === Array || this.queryString[field].split(",").length>1) {
+                        //TODO: Remplacer la vraie value par le @ du prepared statement
                         result+=`${field} IN (${this.queryString[field]})`
                     }
                     else {
+                        //TODO: Remplacer la vraie value par le @ du prepared statement
                         result+=`${field}=@${this.queryString[field]}`
                     }
-                    this.inputs.push({field : this.queryString[field]});
+                    this.inputs[field]=this.queryString[field];
                     result+=" AND ";
                     break;
                 }
@@ -57,7 +65,6 @@ class Query {
 
             }
         }
-        console.log(this.inputs);
         return result.slice(0,-4);
     }
 
@@ -66,22 +73,12 @@ class Query {
     }
 }
 
-//console.log(qs.parse('foo[gt]=bar&bar=a,b,c'))
-
-// console.log(qs.parse('a=c&a=d&a=e'))
-
-// console.log(qs.parse('a=c&a[ne]=b'));
-
-// console.log(qs.parse('a[gt]=c&a[ne]=b'));
-
-// console.log(qs.parse('a=c&a[ne]=b'));
-
 const query1= new Query(["marque", "b"])
 
-const query2= new Query(["a","b"])
+const query2= new Query(["marque","stock"])
 
 //console.log(query1.where("marque=adidas,nike&marque=coca&b[gt]=10"))
-console.log(query2.where("a=c&a=d&b[gt]=10&b[lte]=20"))
+console.log(query2.where("marque=nike&marque=adidas&stock[gt]=10&stock[lte]=20"))
 
 
 export default Query;
