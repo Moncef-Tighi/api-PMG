0bf446f1db93f0c870c69e95a79bd19d9ba164b9
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-03 12:15:30 +0100

    Premier prototype fonctionnel

--- api-cegid/util/parametres.js
+++ api-cegid/util/parametres.js
@@ -1,7 +1,7 @@
 import * as qs from 'qs';
 
 class Query {
-    constructor(fields) {
+    constructor(allowedFields) {
         this.operators = {
             "gt" : ">",
             "gte" : ">=",
@@ -9,7 +9,7 @@ class Query {
             "lt" : '<=',
             "like" : '%' 
         }
-        this.fields = fields || [];
+        this.allowedFields = allowedFields;
         this.queryString="";
     }
 
@@ -23,21 +23,30 @@ class Query {
         this._parse(queryString);
         if (!this.queryString) return "";
         let result = 'WHERE ';
-        console.log(this.queryString);
         for (const field of Object.keys(this.queryString)) {
 
-            if (!field in this.fields) {
+            if (!this.allowedFields.includes(field)) {
                 return `Erreur : le field ${field} n'est pas autorisé`;
             }
+            const fields = this.queryString[field]
 
-            for (const param of Object.keys(this.queryString[field]))
-            if (param[0] in this.operators) {
-                result+=`${param[0]} ${this.operators[param[0]]} @${param[0]}` 
+            for (const param of Object.keys(fields)) {
+                if (param in this.operators) {
+                    result+=`${field} ${this.operators[param]} ${fields[param]}` 
+                }
+                else {
+                    if (this.queryString[field].constructor === Array ) {
+                        result+=`${field} IN (${this.queryString[field]})`
+                        result+= " AND "
+                        break;
+                    }
+                    else {
+                        result+=`${field}=@${this.queryString[field]}`
+                    }
+                }    
+                result+= " AND "
+
             }
-            else {
-                result+=`${param}=@${param}`
-            }
-            result+= " AND "
         }
         return result.slice(0,-4);
     }
@@ -57,12 +66,12 @@ class Query {
 
 // console.log(qs.parse('a=c&a[ne]=b'));
 
-const query1= new Query(["erreur"])
+const query1= new Query(["marque", "b"])
 
-const query2= new Query(["a"])
+const query2= new Query(["marque"])
 
-//console.log(query1.where("a=c&a=d&a=e"))
-console.log(query2.where("a=c&b[gt]=10"))
+console.log(query1.where("marque=nike&marque=coca&b[gt]=10"))
+//console.log(query2.where("a=c&a=d&b[gt]=10&b[lte]=20"))
 
 
 export default Query;
