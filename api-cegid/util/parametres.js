5f7452ce9f9a886677ffaa0bf45e535660560828
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-03 14:34:43 +0100

    Implémentation de l'erreur handeling

--- api-cegid/util/parametres.js
+++ api-cegid/util/parametres.js
@@ -25,7 +25,7 @@ class Query {
 
     _conditions(start) {
 
-        if (Object.keys(this.queryString).length === 0) return "";
+        if (Object.keys(this.queryString).length === 0) return "Empty";
         let result = start;
         for (const field of Object.keys(this.queryString)) {
 
@@ -40,23 +40,27 @@ class Query {
             //Exemple : b>@b AND b<@b ne fonctionnerait pas. Mais b>@1 AND b<@2 (les chiffres sont la valeur de i)fonctionnee
             
             for (const param of Object.keys(fields)) {
+
                 i++;
-                console.log(param);
                 if (param in this.operators) {
+                    if (!fields[param]) return `Erreur de syntax, aucune valuer n'a été trouvé pour ${field}[${param}]`
                     if (param === 'like') result +=`${field} LIKE '%${fields[param]}%'`;
                     //TODO: Remplacer la vraie value par le @ du prepared statement
                     else result+=`${field} ${this.operators[param]} ${fields[param]}`;
                     this.inputs[i]= fields[param];
                 }
                 else {
+                    console.log(this.queryString[field]);
+                    if (typeof this.queryString[field] === 'object') return "Erreur de syntax : La querry n'est pas valide"
+
                     try {
-                        if (this.queryString[field].constructor === Array || this.queryString[field].split(",").length>1) {
+                        if (this.queryString[field].split(",").length>1) {
                             //TODO: Remplacer la vraie value par le @ du prepared statement
                             result+=`${field} IN (${this.queryString[field]})`;
                         }
                         else {
                             //TODO: Remplacer la vraie value par le @ du prepared statement
-                            result+=`${field}=@${this.queryString[field]}`;
+                            result+=`${field}=${this.queryString[field]}`;
                         }    
                     } catch(Err) {
                         //Si l'opérateur est invalide, le code au dessus throw une erreur qu'on catch ici.
@@ -97,13 +101,19 @@ class Query {
 
 const query1= new Query(["marque", "b"]);
 
-const query2= new Query(["marque","stock"]);
+const query2= new Query(["marque","stock", "a", "b", "c"]);
 
-//console.log(query1.where("marque=adidas,nike&marque=coca&b[gt]=10"))
 
-//console.log(query2.where("marque[like]=adi"));
-//console.log(query2.having("marque=adidas,nike&stock[gt]=10&stock[lte]=20"));
+console.log(query1.where("marque=nike&b[gt]=10"))
+// console.log(query2.where("marque[like]=adi"));
+// console.log(query2.having("marque=adidas,nike&stock[gt]=10&stock[lte]=20"));
 //console.log(query2.sanitize(query2.having("marque=adidas,nike&stock[gt]=10&stock[lte]=20")));
-console.log(query2.where('stock[a]=test'));
+
+//Différentes erreurs possibles : 
+// console.log(query2.where('stock[a]=test'));
+// console.log(query2.where(""));
+// console.log(query2.where(["a", "b", 'c']));
+// console.log(query2.where("InvalidField[gt]=5"));
+// console.log(query2.where("stock=&stock[gt]=5"));
 
 export default Query;
