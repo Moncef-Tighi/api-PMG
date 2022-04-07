1a38ec78922fde228cb81aee7e5beeb4d5c9d975
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-07 18:54:48 +0100

    Handeling invalid operators

--- api-cegid/util/parametres.js
+++ api-cegid/util/parametres.js
@@ -30,14 +30,24 @@ class Query {
         //2-Faire passer chaque morceau dans la méthode _conditions comme si c'était des querry à part
         //3-Modifier _conditions pour ignorer les parenthéses là ou il faut et juste les output normal dans le SQL
         //4-Réassembler les morceaux avec un " OR " entre eux.
-        this.queryString = queryString        
-        if (Object.keys(this.queryString).length === 0) return "";
-        
-        if (qs.stringify(queryString).includes("[or]")) {
+        if (string.includes("[or]")) {
             const [before, after] = this._splitInTwo(current, current.search(field));
             return `${before}(${ after.replace("[or]", " OR ") })`;            
         }
         return
+    }
+
+    _parsing(queryString) {
+        this.queryString = queryString        
+        if (Object.keys(this.queryString).length === 0) return "";
+        const regex = /\[([^\]]+)\]/g
+        const string = qs.stringify(queryString, { encode: false, indices: false  })
+        const operators = string.match(regex);
+        for (const operator of operators) {
+            if (operator = '[or]') this._or(queryString);
+            if (!(operator.slice(1,-1) in this.operators)) throw new Error("Erreur : Opérateur Invalide");
+        }
+
     }
 
     _conditions(start) {
@@ -45,25 +55,18 @@ class Query {
         for (const field of Object.keys(this.queryString)) {
             if (this.reservedKeyWords.includes(field)) continue;
             const fields = this.queryString[field]
-            //console.log( Object.values(fields)[0].includes('[OR]') );
-            // try {
-            //     if( Object.values(fields)[0].includes('[or]') || fields.includes('[or]')) {
-            //     };
-            // } catch(error) {
-            //     //ça n'a aucucn sens le catch vide. Mais pour une raison inconnu l'optional chaining (?) ne fonctionne pas
-            // }
+
             if (this.excluedFields.includes(field)) {
-                return `Erreur : le field ${field} n'est pas autorisé`;
+                throw new Error(`Erreur : le field ${field} n'est pas autorisé`);
             }
             let i=0;
             //Le i est nécessaire pour protéger les inputs contre les attaques SQL
             //le i sert de nom aux inputs dans le cas ou le même attribut a plusieurs condition
             //Exemple : b>@b AND b<@b ne fonctionnerait pas. Mais b>@1 AND b<@2 fonctionne (les chiffres sont la valeur de i)
             for (const param of Object.keys(fields)) {
-                console.log(param);
                 i++;
                 if (param in this.operators) {
-                    if (!fields[param]) return `Erreur de syntax, aucune valeur n'a été trouvé pour ${field}[${param}]`
+                    if (!fields[param]) throw new Error( `Erreur de syntax, aucune valeur n'a été trouvé pour ${field}[${param}]`);
                     if (param === 'like') {
                         result +=`${field} LIKE @${i} `;
                         //On est obligé d'inclure les " % " Après parce que le parser de MSSQL ne les gère pas
@@ -75,25 +78,19 @@ class Query {
                     }
                 }
                 else {
-                    try {
                         const params = this.queryString[field]
-                        if (typeof this.queryString[field] === 'string' || this.queryString[field] instanceof String) {
+                        if (typeof params === 'string' || params instanceof String) {
                             result+=`${field}=@${field}`;
-                            this.inputs[field]=this.queryString[field];
-                        }
-                        else {
+                            this.inputs[field]=params;
+                        }else{
                             //TODO: Remplacer la vraie value par le @ du prepared statement
                             let inOperator = "";
                             for (let y=0; y<params.length;y++) {
                                 inOperator+= `@${y+999},`;
-                                this.inputs[y+999]=this.queryString[field][y];
+                                this.inputs[y+999]=params[y];
                             }
                             result+=`${field} IN (${inOperator.slice(0,-1)})`;
                         }
-                    } catch(err) {
-                        //Si l'opérateur est invalide, le code au dessus throw une erreur qu'on catch ici.
-                        return "Opérateur invalide "
-                    }
                     result+=" AND ";
                     break;
                 }
@@ -104,12 +101,12 @@ class Query {
     }
 
     where(queryString) {
-        this._or(queryString);
+        this._parsing(queryString);
         return this._conditions(" WHERE ");
     }
 
     having(queryString) {
-        this._or(queryString);
+        this._parsing(queryString);
         return this._conditions(" HAVING ");
     }
 
@@ -141,7 +138,7 @@ class Query {
         const fields = this.queryString.sort.split(',');
         let result = "ORDER BY "
         fields.forEach(field=>{
-            if (this.excluedFields.includes(field)) return `Erreur : le field ${field} n'est pas autorisé`;
+            if (this.excluedFields.includes(field)) throw new Error(`Erreur : le field ${field} n'est pas autorisé`);
             if (field[0] === "-") return result+= `${field.slice(1)} DESC,`
             else if (field[0]==="+") return result+= `${field.slice(1)} ASC,`
             else return result+= `${field} ASC,`
@@ -170,9 +167,9 @@ const query2= new Query("",["invalidField"]);
 
 //Des querry qui doivent être valides pour WHERE: 
 
-// console.log(query1.where(qs.parse("marque=nike&a[gt]=10")))
+// console.log(query1.where(qs.parse("marque=nike&marque=adidas&a[gt]=10")))
 // console.log(query2.having(qs.parse("marque=adidas,nike&stock[gt]=10&stock[lte]=20")));
-// console.log(query2.where(qs.parse("stock=10&b=20&a[gt]=10")));
+// console.log(query2.where(qs.parse("stock[lt]=10&b=20&a[gt]=10")));
 
 
 // console.log(query2.where("stock[lt]=10&[or]&stock[gt]=20"));
@@ -195,11 +192,11 @@ const query2= new Query("",["invalidField"]);
 
 //Des querry qui doivent être invalides : 
 
-// console.log(query2.where('stock[a]=test'));
-// console.log(query2.where(""));
-// console.log(query2.where(["a", "b", 'c']));
-// console.log(query2.where("InvalidField[gt]=5"));
-// console.log(query2.where("stock=&stock[gt]=5"));
+// console.log(query2.where(qs.parse('stock[a]=test')));
+console.log(query2.where(qs.parse('')));
+// console.log(query2.where(qs.parse(["a", "b", 'c'])));
+// console.log(query2.where(qs.parse("InvalidField[gt]=5")));
+// console.log(query2.where(qs.parse("stock=&stock[gt]=5")));
 
 
 //Des querry vides : 
