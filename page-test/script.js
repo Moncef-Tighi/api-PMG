66de9b4fffacb21045e9cba773401d0d51b299fe
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-12 14:39:16 +0100

    Fix de quelques problèmes de rendering

--- page-test/script.js
+++ page-test/script.js
@@ -5,37 +5,56 @@ const emptyPage = document.querySelector('#empty');
 const errorText=document.querySelector('#textError');
 const form = document.querySelector('#form');
 const codeArticle = document.querySelector('#codeArticle');
-const table = document.querySelector('#table');
 const sousTitre = document.querySelector('#label');
-
-/*
-    Span de la partie détails article
-*/
-
-const prix_initial = document.querySelector('#prix_initial');
-const prix_acutel = document.querySelector('#prix_acutel');
-const famille_niv1 = document.querySelector('#famille_niv1');
-const famille_niv2 = document.querySelector('#famille_niv2');
-const date_creation = document.querySelector('#date_creation');
-const libre_art = document.querySelector('#libre_art');
-const date_tarif = document.querySelector('#date_tarif');
-const description_tarif = document.querySelector('#description_tarif');
-const date_debut = document.querySelector('#date_debut');
-const date_fin = document.querySelector('#date_fin');
-const type_tarif = document.querySelector('#type_tarif');
-const nature_tarif = document.querySelector('#nature_tarif');
-const pertarif = document.querySelector('#pertarif');
-
-//---------------------------------------
+const codeBarre = document.querySelector('#codeBarre');
+const stockNet = document.querySelector('#stockNet');
+const dimension = document.querySelector('#dimension');
 
 
 const pageCleanup= function() {
     emptyPage.style='none';
     mainPage.style='none';
     label.innerText="";
-    table.innerText="";
-
+    codeBarre.innerHTML='<td>Code Barre</td>'
+    stockNet.innerHTML='<td>Stock Net </td>'
+    dimension.innerHTML=`<td>Dimension</td>`
 }
+
+
+const ficheArticle = function(details) {
+    /*
+    Span de la partie détails article
+*/
+
+    const prix_initial = document.querySelector('#prix_initial');
+    const prix_acutel = document.querySelector('#prix_acutel');
+    const famille_niv1 = document.querySelector('#famille_niv1');
+    const famille_niv2 = document.querySelector('#famille_niv2');
+    const date_creation = document.querySelector('#date_creation');
+    const libre_art = document.querySelector('#libre_art');
+    const date_tarif = document.querySelector('#date_tarif');
+    const description_tarif = document.querySelector('#description_tarif');
+    const date_debut = document.querySelector('#date_debut');
+    const date_fin = document.querySelector('#date_fin');
+    const type_tarif = document.querySelector('#type_tarif');
+    const nature_tarif = document.querySelector('#nature_tarif');
+    const pertarif = document.querySelector('#pertarif');
+
+    prix_acutel.innerText = details.prixActuel;
+    prix_initial.innerText = details.prixInitial;
+    famille_niv1.innerText = details.GA_FAMILLENIV1;
+    famille_niv2.innerText = details.GA_FAMILLENIV2;
+    date_tarif.innerText = details.dernierTarif.split("T")[0];
+    description_tarif.innerText = details.descriptionTarif;
+    date_creation.innerText = details.GA_DATECREATION.split("T")[0];
+    libre_art.innerText = details.GA2_LIBREARTE;
+    date_debut.innerText = details.GF_DATEDEBUT.split("T")[0];
+    date_fin.innerText = details.GF_DATEFIN.split("T")[0];
+    type_tarif.innerText = details.GFM_TYPETARIF;
+    nature_tarif.innerText = details.GFM_NATURETYPE;
+    pertarif.innerText = details.GFM_PERTARIF;
+}
+
 
 form.addEventListener('submit', async (event)=> {
     event.preventDefault();
@@ -61,29 +80,24 @@ form.addEventListener('submit', async (event)=> {
     const details = data.body.details;
     mainPage.style.display="grid";
     label.innerText= `Libelle : ${details.GA_LIBELLE}`
-    tailles.forEach((taille => {
-        table.insertAdjacentHTML('afterbegin', 
-        `<tr>
-            <td>${taille.GA_CODEBARRE}</td>
-            <td>${taille.Dimension}</td>
-            <td>${taille["Stock Net"]}</td>
-            <td>${taille.Vendu}</td>
-            <td>${taille.Transfert}</td>
 
-        </tr>`)
+    tailles.forEach((taille => {
+        dimension.insertAdjacentHTML('beforeend', 
+        `<td>${taille.dimension}</td>
+        `)
+        codeBarre.insertAdjacentHTML('beforeend', 
+        `<td>${taille.GA_CODEBARRE}</td>
+        `)
+        stockNet.insertAdjacentHTML('beforeend', `
+        <td>${taille.stockNet}</td>`)
+    
     }))
 
-    prix_acutel.innerText = details.prixActuel;
-    prix_initial.innerText = details.prixInitial;
-    famille_niv1.innerText = details.GA_FAMILLENIV1;
-    famille_niv2.innerText = details.GA_FAMILLENIV2;
-    date_tarif.innerText = details.dernierTarif.split("T")[0];
-    description_tarif.innerText = details.descriptionTarif;
-    date_creation.innerText = details.GA_DATECREATION.split("T")[0];
-    libre_art.innerText = details.GA2_LIBREARTE;
-    date_debut.innerText = details.GF_DATEDEBUT.split("T")[0];
-    date_fin.innerText = details.GF_DATEFIN.split("T")[0];
-    type_tarif.innerText = details.GFM_TYPETARIF;
-    nature_tarif.innerText = details.GFM_NATURETYPE;
-    pertarif.innerText = details.GFM_PERTARIF;
+    ficheArticle(details);
+})
+
+const depot = document.querySelector('#depot');
+
+depot.addEventListener('click', ()=> {
+
 })
