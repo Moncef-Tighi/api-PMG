1df604dbee5f39934b8a5dca60e33616feefac32
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-14 12:01:18 +0100

    Ajout de l'option de filtrage

--- api-cegid/public/recherche.js
+++ api-cegid/public/recherche.js
@@ -26,6 +26,8 @@ form.addEventListener('submit',event => {
     const stock = document.querySelector('#stock').value;
     const pageSize = document.querySelector('#pageSize').value;
     const page = document.querySelector('#page').value;
+    const ordre = document.querySelector('#ordre').value;
+    const sort = document.querySelector('#sort').value;
 
     let result = ''
     if (libelle) result+= `GA_LIBELLE[like]=${libelle}&`
@@ -34,6 +36,7 @@ form.addEventListener('submit',event => {
     if (stock) result+=numberSearch("stock", stock);
     if (pageSize) result+= `pageSize=${pageSize}`
     if (page) result+= `page=${page}`
+    result+=`sort=${ordre}${sort}`
     
     console.log(result);
     window.location.href = decodeURI(`${window.location.href.split('?')[0]}?${result}`);
