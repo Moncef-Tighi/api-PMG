92057fb1758dfeb6d9eaddcac1c77a3d4a7c8073
Moncef-Tighi <monceftighiouart@gmail.com>
2022-04-17 15:27:25 +0100

    Nouvelle requête pour les derniers tarifs

--- api-cegid/models/tarifs.js
+++ api-cegid/models/tarifs.js
@@ -2,12 +2,41 @@ import db from "./database.js";
 import Query from '../util/parametres.js';
 import qs from "qs";
 
+const query= new Query('-Gf_DATEMODIF');
+
+export const tarifsArticles = async function(articles) {
+
+    const sql = `
+    SELECT DISTINCT
+    GA_CODEARTICLE,ISNULL(GF_PRIXUNITAIRE, GA_PVTTC) as 'prixActuel'
+    ,GF_DATEMODIF    
+    FROM GCTARFCONMODEART  
+    WHERE (
+        GF_REGIMEPRIX = 'TTC' AND (
+            (GA_STATUTART='GEN' or GA_STATUTART='UNI')  
+            AND ( GFM_TYPETARIF IS NULL OR GFM_TYPETARIF IN ('','','001','RETAIL')
+            ) 
+        AND GF_ARTICLE<>'') 
+        AND GFM_NATURETYPE = 'VTE' 
+    )
+    ${query.where(qs.parse('GA_CODEARTICLE='+ articles.join('&GA_CODEARTICLE=')))}
+    GROUP BY GA_CODEARTICLE
+    ${query.sort()}`;
+
+    const request = new db.Request()
+    query.sanitize(request);
+    const data = await request.query(sql);
+    return data.recordset;
+
+};
+
+
 export const articleAllTarifs = async function(article) {
 
     const data = await db.query`
     SELECT DISTINCT
-    GA_CODEARTICLE,GA_LIBELLE,ISNULL(GF_PRIXUNITAIRE, GA_PVTTC) as 'Prix Actuel', GA_PVTTC as 'Prix Initial',
-    GF_DATEMODIF, GF_LIBELLE as 'Description Tarif', GF_DATEDEBUT, GF_DATEFIN
+    GA_CODEARTICLE,GA_LIBELLE,ISNULL(GF_PRIXUNITAIRE, GA_PVTTC) as 'prixActuel', GA_PVTTC as 'prixInitial',
+    GF_DATEMODIF, GF_LIBELLE, GF_DATEDEBUT, GF_DATEFIN
     GFM_TYPETARIF, GFM_PERTARIF, GFM_NATURETYPE,
     GA2_LIBREARTE
     
