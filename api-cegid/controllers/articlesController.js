a0986d29402c8d077d97cd4449bf77adb193858e
root <root@LAPTOP-1PCUB1QS.localdomain>
2022-04-24 21:54:54 +0100

    bug fix : L'histoire des tarifs avait un lien invalide

--- api-cegid/controllers/articlesController.js
+++ api-cegid/controllers/articlesController.js
@@ -42,7 +42,7 @@ export const unArticle = catchAsync(async function(request, response, next) {
         status : "ok",
         codeArticlce : request.params.article,
         details_stock : encodeURI(`http://${request.get('host')}/api/v1/articles/detail_stock/${request.params.article}`),
-        historiqueTarif : encodeURI(`http://${request.get('host')}/api/v1/articles/tarifs/${request.params.article}`),
+        historiqueTarif : encodeURI(`http://${request.get('host')}/api/v1/tarifs/${request.params.article}`),
         body : {
             "info" : infoArticle[0],
             taille,
