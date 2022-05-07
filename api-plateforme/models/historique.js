58712cdc95b05780682786f6d71335caee8fa940
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-07 16:17:20 +0100

    Modification de la table d'historique pour ajouter les foreign key de l'objet affecté

---
+++ api-plateforme/models/historique.js
@@ -0,0 +1,13 @@
+import db from "./database.js";
+
+export const createAction = async function(id_employe, action_sur,categorie,type,description ){
+    const sql = `
+    INSERT INTO historique_actions(id_employe, action_sur, categorie, date_creation,type,description)
+    VALUES 
+    ($1, $2, $3, CURRENT_TIMESTAMP,$5, $4 )
+    RETURNING *
+    `
+    const values = [id_employe, action_sur, categorie,type,description];
+    const response = await db.query(sql, values)
+    return response.rows[0];
+}
