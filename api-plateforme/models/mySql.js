cbed299a406a7c9f447a2cb067fbd3e18993d8fc
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-08 14:46:03 +0100

    Ajout de fichiers de config vide en privision du déploiement

--- api-plateforme/models/mySql.js
+++ api-plateforme/models/mySql.js
@@ -1,11 +1,16 @@
 import mysql from "mysql2/promise";
 // create the connection to database
+try {
 
-const db = mysql.createPool({
-    host: process.env.MY_HOST,
-    user: process.env.MY_USER,
-    password: process.env.MY_PASSWORD,
-    database: process.env.MY_DATABASE
-});
+    var db = mysql.createPool({
+        host: process.env.MY_HOST,
+        user: process.env.MY_USER,
+        password: process.env.MY_PASSWORD,
+        database: process.env.MY_DATABASE
+    });
+    console.log("Connexion à la base de donnée de WordPress réussie");
+} catch(error) {
+    console.error(error);
+}
 
 export default db
