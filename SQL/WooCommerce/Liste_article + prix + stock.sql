1ade8ed623a96e054c399f485bf297fd23ddd1ae
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-08 12:52:45 +0100

    correction des requêtes d'update

--- SQL/WooCommerce/Liste_article + prix + stock.sql
+++ SQL/WooCommerce/Liste_article + prix + stock.sql
@@ -1,6 +1,6 @@
 SELECT ID,post_title, post_name, post_content, post_status,meta_key, meta_value FROM `wp_posts` 
 INNER JOIN wp_postmeta ON wp_postmeta.post_id=wp_posts.ID AND 
-	(wp_postmeta.meta_key="_stock_status" OR wp_postmeta.meta_key="_price")
+	(wp_postmeta.meta_key="_stock_status" OR wp_postmeta.meta_key="_price" OR wp_postmeta.meta_key="_sku")
 WHERE post_type ="product"
 -- AND ID="12"
 
--- SQL/WooCommerce/update article.sql
+++ SQL/WooCommerce/update article.sql
@@ -1,9 +1,14 @@
 UPDATE  wp_posts
 INNER JOIN wp_postmeta ON wp_postmeta.post_id=wp_posts.ID AND wp_postmeta.meta_key="_stock_status"
-SET wp_postmeta.meta_value="dispo"
-WHERE wp_posts.post_type ="product" AND wp_posts.ID="12"
+SET wp_postmeta.meta_value="outofstock"
+WHERE wp_posts.post_type ="product" AND wp_postmeta.post_id = (
+    SELECT post_id from wp_postmeta WHERE wp_postmeta.meta_value="CODE ARTICLE"
+)
+--C'est soit instock ou outofstock
 
 UPDATE  wp_posts
 INNER JOIN wp_postmeta ON wp_postmeta.post_id=wp_posts.ID AND  wp_postmeta.meta_key="_price"
 SET wp_postmeta.meta_value=2500
-WHERE wp_posts.post_type ="product" AND wp_posts.ID="12"
+WHERE wp_posts.post_type ="product" AND wp_postmeta.post_id = (
+    SELECT post_id from wp_postmeta WHERE wp_postmeta.meta_value="CODE ARTICLE"
+)
