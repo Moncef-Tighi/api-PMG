497cc57ca3066a8f71ba21550a0d097d51a5d9bf
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-09 15:55:52 +0100

    Connexion avec le backend pour connecter l'user

--- plateforme/src/components/LoginForm.js
+++ plateforme/src/components/LoginForm.js
@@ -2,17 +2,41 @@ import classes from "./LoginForm.module.css";
 import {InputLabel, OutlinedInput, InputAdornment,IconButton, Button} from '@mui/material';
 import {Visibility, VisibilityOff, AccountCircle } from '@mui/icons-material'
 import { useState } from "react";
+import axios from 'axios';
 
 const LoginForm = function() {
     const [showPassword, setPasswordVisibility]=useState(false);
-    
+    const [error, setError] = useState("");
+
     const handleClickShowPassword = () => {
         setPasswordVisibility(!showPassword);
       };
     
-      const handleMouseDownPassword = (event) => {
+    const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+    };
+
+    const login = async (event)=> {
         event.preventDefault();
-      };
+        const {password, email}= event.currentTarget.elements
+
+        if(password.value.trim()==="" || email.value.trim()==="") return setError("L'email et le mot de passe ne peuvent pas être vide");
+
+        try {
+            const response = await axios.post('http://localhost:4001/api/v1/connexion', {
+                email: email.value,
+                password: password.value
+            })
+            console.log(response);        
+            setError("");
+        } catch(error) {
+            if (error.response) {
+                if (error.response.status===401) setError(error.response.data.message);
+            } else {
+                setError("Impossible de se connecter au serveur");
+            }
+        }
+    }
     
 
     return (
@@ -20,7 +44,8 @@ const LoginForm = function() {
             <div className={classes.positionning}>
                 <img src="logo-pmg.png"></img>
                 <h1>S'identifier</h1>
-                <form>
+                <h3>{error}</h3>
+                <form onSubmit={login}>
                     <InputLabel htmlFor="email">Email</InputLabel>
                     <OutlinedInput
                     id="email"
@@ -47,7 +72,8 @@ const LoginForm = function() {
                     </InputAdornment>}
                     />
             
-                    <Button color="primary" variant="contained" size="large" sx={ {marginTop: "50px"}} >
+                    <Button color="primary" variant="contained" 
+                    size="large" sx={ {marginTop: "40px", backgroundColor: "#E92129"}} type="submit">
                         Confirmer</Button>
                 </form>
 
--- plateforme/src/theme.js
+++ plateforme/src/theme.js
@@ -6,6 +6,10 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  primary: {
+    main: '#E92129',
+    darker: '#DF161D',
+  },
   palette: {
     primary: {
       main: '#E92129',
