2f6fd4bbef0905bc233dbd5ede59bfbec50825b2
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-11 15:29:04 +0100

    Design du menu de navigation

--- plateforme/src/components/Navigation.js
+++ plateforme/src/components/Navigation.js
@@ -1,18 +1,41 @@
 import classes from './Navigation.module.css'
 
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+
+
 const Navigation = function() {
 
     return (
         <nav>
-            <ul>
-                <li>Articles</li>
-                <li>Commandes</li>
-                <li>Prix</li>
-                <li>Dimension</li>
-                <li>Employes</li>
-                <li>Permissions</li>
-                <li>Historique</li>
-            </ul>
+            <h1>Menu</h1>
+            <List
+            sx={{ width: '100%'}}
+            component="nav"
+            aria-labelledby="nested-list-subheader">
+            <ListItemButton sx={{ width: '100%', maxHeight: "60px"}}>
+                <ListItemIcon><SendIcon  sx={{width:'1.4em', height: '1.4em', color: '#262626'}}/> </ListItemIcon>
+                <ListItemText primary="send email" />
+            </ListItemButton>
+            <ListItemButton sx={{ width: '100%', maxHeight: "60px"}}>
+                <ListItemIcon>
+                <DraftsIcon  sx={{width:'1.4em', height: '1.4em', color: '#262626'}}/>
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+            </ListItemButton>
+            <ListItemButton sx={{ width: '100%', maxHeight: "60px"}}>
+                <ListItemIcon>
+                <InboxIcon  sx={{width:'1.4em', height: '1.4em', color: '#262626'}}/>
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+            </ListItemButton>
+            </List>
+
         </nav>
     )
 }
