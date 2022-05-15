610738786aba6b63692a5dca98d35918bbb8b0f0
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-15 11:53:19 +0100

    Refactoring pour séparer le component des tailles de celui des depots

---
+++ plateforme/src/components/DepotTable.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+export const DepotTable = function ({ taille, stock }) {
+    const find = [];
+    for (const depot of Object.keys(stock)) {
+        const current = stock[depot].filter(depot => depot.dimension === taille.dimension);
+        find[depot] = { dimension: current[0]?.dimension, stock: current[0]?.stockNet };
+    }
+    const [open, setOpen] = React.useState(false);
+
+    return (
+
+        <>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {taille.GA_CODEBARRE}
+                </TableCell>
+                <TableCell align="right">{taille.dimension}</TableCell>
+                <TableCell align="right">{taille.stockNet}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Depot</TableCell>
+                                        <TableCell align="right">Stock</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {Object.keys(find).map(depot => {
+                                        console.log(depot);
+                                        console.log(find[depot]);
+                                        return (<TableRow key={depot}>
+                                            <TableCell component="th" scope="row">
+                                                {depot}
+                                            </TableCell>
+                                            <TableCell align="right">{find[depot].stock}</TableCell>
+                                        </TableRow>);
+                                    }
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
+};
--- plateforme/src/components/TailleTable.js
+++ plateforme/src/components/TailleTable.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,72 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-
-const DepotTable = function ({taille, stock}) {
-    console.log(taille);
-    console.log(stock);
-    const find = [];
-    for (const depot of Object.keys(stock)) {
-        const current = stock[depot].filter(depot => depot.dimension===taille.dimension)
-        find[depot] = {dimension : current[0]?.dimension, stock : current[0]?.stockNet};
-    }
-    console.log(find)
-    const [open, setOpen] = React.useState(false);
-
-    return (
-
-        <>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-            <TableCell>
-            <IconButton
-                aria-label="expand row"
-                size="small"
-                onClick={() => setOpen(!open)}
-            >
-                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-            </TableCell>
-            <TableCell component="th" scope="row">
-            {taille.GA_CODEBARRE}
-            </TableCell>
-            <TableCell align="right">{taille.dimension}</TableCell>
-            <TableCell align="right">{taille.stockNet}</TableCell>
-        </TableRow>
-        <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <Box sx={{ margin: 1 }}>
-                <Table size="small" aria-label="purchases">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Depot</TableCell>
-                        <TableCell align="right">Stock</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                     {Object.keys(find).map( depot=> {
-                         console.log(depot);
-                         console.log(find[depot]);
-                        return(<TableRow key={depot}>
-                        <TableCell component="th" scope="row">
-                            {depot}
-                        </TableCell>
-                        <TableCell align="right">{find[depot].stock}</TableCell>
-                        </TableRow>)
-                     }
-                    )} 
-                    </TableBody>
-                </Table>
-                </Box>
-            </Collapse>
-            </TableCell>
-        </TableRow>
-        </>
-    )
-}
+import { DepotTable } from './DepotTable';
 
 
 const TailleTable = function({tailles, stock}) {
