198530f093ccca3457837aae13b667c2e40b3a2c
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-15 15:23:34 +0100

    Formulaire de création des employés

--- plateforme/src/components/Employés/AddEmployes.js
+++ plateforme/src/components/Employés/AddEmployes.js
@@ -1,22 +1,75 @@
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import classes from './AddEmployes.module.Css'
-const AddEmployes = function({open, onClose}) {
+import classes from './AddEmployes.module.css'
+import { OutlinedInput, IconButton, InputAdornment, InputLabel, Button } from '@mui/material';
+import { useState } from 'react';
+import {Visibility, VisibilityOff} from '@mui/icons-material'
+
+const AddEmployes = function({open, onClose, createEmploye}) {
+    const [showPassword, setPasswordVisibility]=useState(false);
+
+    const handleClickShowPassword = () => {
+        setPasswordVisibility(!showPassword);
+      };
+    
+    const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+    };  
 
     return (
+        <>
         <Modal
             open={open}
             onClose={onClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
         >
             <Box className={classes.modal}>
-                <form>
-                    
+                    <h1>Ajouter un Employé</h1>
+                    <form onSubmit={createEmploye}> 
+                        <InputLabel htmlFor="nom">Nom</InputLabel>
+                        <OutlinedInput id='nom' color='primary' size='small' fullWidth={true} required/>
+                        <InputLabel htmlFor="prenom">prenom</InputLabel>
+                        <OutlinedInput id='prenom' color='primary' size='small' fullWidth={true} required/>
+                        <InputLabel htmlFor="email">E-Mail</InputLabel>
+                        <OutlinedInput id='email' color='primary' size='small' fullWidth={true} required/>
+                        
+                        <InputLabel htmlFor="poste">Poste</InputLabel>
+                        <OutlinedInput id='poste' color='primary' size='small' fullWidth={true} required/>
+
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <OutlinedInput
+                        size='small'
+                        id="password"
+                        type={showPassword ? 'text' : 'password'}
+                        fullWidth={true}
+                        endAdornment={<InputAdornment position="end">
+                        
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>}
+                        />
+
+
+                    <div className={classes.flex}>
+                    <Button color="primary" variant="contained" fullWidth={true}
+                    size="large" type="submit">
+                    Confirmer</Button>
+
+                    <Button variant="contained" color='primaryLighter'
+                    size="large" onClick={onClose} sx={{marginLeft: "10px"}}>
+                    Annuler</Button>
+                    </div>
                 </form>
+                
             </Box>
         </Modal>
+
+        </>
         )
 }
 
