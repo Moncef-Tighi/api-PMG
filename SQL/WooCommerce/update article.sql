ba10dc714ac215de25a79bad6b1fb2a487cf146c
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-17 22:34:58 +0100

    Mise à jour des requêtes de WooCommerce pour prendre en compte les posts variants

--- SQL/WooCommerce/update article.sql
+++ SQL/WooCommerce/update article.sql
@@ -4,7 +4,7 @@ SET wp_postmeta.meta_value="outofstock"
 WHERE wp_posts.post_type ="product" AND wp_postmeta.post_id = (
     SELECT post_id from wp_postmeta WHERE wp_postmeta.meta_value="CODE ARTICLE"
 )
---C'est soit instock ou outofstock
+--C'est soit instock ou outofstock MAIS Y A AUSSI UN "onbackorder"
 
 UPDATE  wp_posts
 INNER JOIN wp_postmeta ON wp_postmeta.post_id=wp_posts.ID AND  wp_postmeta.meta_key="_price"
--- SQL/WooCommerce/vente article.sql
+++ SQL/WooCommerce/vente article.sql
@@ -1,1 +1,10 @@
 SELECT sku, onsale, total_sales FROM wp_wc_product_meta_lookup
+
+
+
+SELECT ID,post_title, post_name, post_content, post_status,meta_key, meta_value,sku, onsale, total_sales FROM `wp_posts` 
+INNER JOIN wp_postmeta ON wp_postmeta.post_id=wp_posts.ID AND 
+	(wp_postmeta.meta_key="_stock_status" OR wp_postmeta.meta_key="_price" OR wp_postmeta.meta_key="_sku")
+INNER JOIN wp_wc_product_meta_lookup ON wp_wc_product_meta_lookup.product_id=wp_posts.ID
+WHERE post_type ="product" AND sku="CODE ARTICLE"
+ORDER BY total_sales DESC
