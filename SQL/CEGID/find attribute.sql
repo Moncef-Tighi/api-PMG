4297700f64d80672125fe364d94fc8f5b57797f0
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-24 09:39:25 +0100

    Requête pour trouver un attribut n'importe où dans la DB

---
+++ SQL/CEGID/find attribute.sql
@@ -0,0 +1,7 @@
+SELECT      c.name  AS 'ColumnName'
+            ,t.name AS 'TableName'
+FROM        sys.columns c
+JOIN        sys.tables  t   ON c.object_id = t.object_id
+WHERE       c.name LIKE '%LibreArt%'
+ORDER BY    TableName
+            ,ColumnName;
