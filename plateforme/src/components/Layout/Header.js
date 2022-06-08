617fc4be30789c9270602b7377a958724324800f
Moncef_Tighi <monceftighiouart@gmail.com>
2022-06-08 16:01:54 +0100

    Redesign du Header

--- plateforme/src/components/Layout/Header.js
+++ plateforme/src/components/Layout/Header.js
@@ -5,13 +5,24 @@ import icon from './pmg-icon.svg'
 import AuthContext from '../../state/AuthContext';
 import { IconButton } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
+import Navigation from './Navigation';
+import React from 'react';
+import { Button, Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const Header = function() {
     const [nom, setNom] = useState(null)
     const [prenom, setPrenom] = useState(null)
     const [poste, setPoste] = useState(null)
     const navigate= useNavigate();
-
+    const [menu, setMenu] = useState(false);
+    const toggleDrawer = (openMenu) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+        setMenu(!menu);
+      };
 
     const authContext = useContext(AuthContext)
     const employe = authContext.employe;
@@ -29,13 +40,29 @@ const Header = function() {
     return (
         <header>
             <div>
-                <img className={classes.icon} src={icon} alt="small-logo"></img>
-                <div className={classes.name}>
-                <Link to='/me'><h2>{nom} {prenom}</h2></Link>
-                    <h4>{poste}</h4>
-                </div>
+                
+                <React.Fragment key={"left"}>
+                <Button onClick={toggleDrawer(true)}><MenuIcon sx={{fontSize : "2.5em", marginRight: "10px"}}/></Button>
+                <Drawer
+                    anchor={"left"}
+                    open={menu}
+                    onClose={toggleDrawer(false)}
+                >
+                    <Navigation toggleDrawer={toggleDrawer}/>
+                </Drawer>
+                </React.Fragment>
+
+                <img className={classes.icon} src="logo-pmg.png" alt="small-logo"></img>
             </div>
+
+            <div>
+            <div className={classes.name}>
+            <Link to='/me'><h2>{nom} {prenom}</h2></Link>
+                <h4>{poste}</h4>
+            </div>
+            <NotificationsIcon sx={{color: "grey"}}/>
             <IconButton onClick={logoutHandeler}><LogoutIcon/></IconButton>
+            </div>
         </header>
     )
 }
