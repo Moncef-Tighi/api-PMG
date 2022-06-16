45ee6acc78496c662b7097fdc490255d5d6ef8a9
Moncef_Tighi <monceftighiouart@gmail.com>
2022-06-16 09:40:04 +0100

    Nouveau script d'insertion des données avec update des status

--- SQL/Plateforme/testData.sql
+++ SQL/Plateforme/testData.sql
@@ -11,6 +11,9 @@ INSERT INTO employee(email, password, nom, date_creation)
 VALUES 
 ('admin', '$2b$10$tEbxTV47jJT2tWWGa/XFWep/dsOBbRtgOsqPLzkCxAgxZ6JH5GvbG' , 'admin',  CURRENT_TIMESTAMP );
 
+--Attention ! L'insertions des permissions peut changer si il y a eu des insertions avant sur la base de donnée
+--et que donc les chiffres actuellement spécifiée ne sont pas correct
+
 INSERT INTO permissions(id_employe, id_role) VALUES
 (1,1),
 (1,2);
@@ -27,7 +30,7 @@ VALUES
 (3, 2, 'Abou El Hassan'),
 (4,2, 'El Karimia');
 
-INSERT INTO baladia(numero_baladia, numero_daira, nom_baladia)
+INSERT INTO commune(numero_commune, numero_daira, nom_commune)
 VALUES
 (1,1,'Bab El-Oued'),
 (2,1,'Casbah'),
@@ -42,10 +45,29 @@ VALUES
 (9,3,'Sidi Moussa'),
 
 
-(7,4,'Abou El Hassan'),
-(8,4,'Talassa'),
-(9,4,'Tadjena'),
+(10,4,'Abou El Hassan'),
+(11,4,'Talassa'),
+(12,4,'Tadjena'),
 
-(7,5,'El Karimia'),
-(8,5,'Harchoun'),
-(9,5,'Beni Bouateb');
+(13,4,'El Karimia'),
+(14,4,'Harchoun'),
+(15,4,'Beni Bouateb');
+
+
+-- Les status d'une commande sont statiques
+
+INSERT INTO liste_status_commande(status)
+VALUES
+
+('Annulée'),
+('Reçu'),
+('Nouvelle Commande'),
+('Attribuée à employé'),
+('Confimée'),
+('En Attente des magasins'),
+('En cours de packaging'),
+('En cours de ramassage'),
+('En cours de livraison'),
+('Echange'),
+('Réussie'),
+('Echouée');
