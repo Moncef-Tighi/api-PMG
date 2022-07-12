d6f27f86b86a382f4c3227898d32c116779c2687
Moncef_Tighi <monceftighiouart@gmail.com>
2022-07-12 09:25:44 +0100

    bugfix : L'attribution de commande ne sélectionnait pas la toute dernière attribution en date

--- api-plateforme/models/commande_attribution.js
+++ api-plateforme/models/commande_attribution.js
@@ -6,7 +6,9 @@ export const getCommandeAttribution = async function(id) {
         SELECT commande.id_commande, id_employe, date_attribution, id_status  FROM commande
         LEFT OUTER JOIN commande_attribution ON commande_attribution.id_commande = commande.id_commande
         LEFT OUTER JOIN status_commande ON status_commande.id_commande = commande.id_commande
-        WHERE commande.id_commande = ${id}
+        WHERE commande.id_commande = ${id} AND date_attribution = 
+		(SELECT MAX(date_attribution) FROM commande_attribution
+		WHERE commande.id_commande = ${id})
     `
     const response = await db.query(sql)
     return response.rows[0];
