c61d4f96a6b21dbbef0ab0b36694cc7d5759344f
Moncef_Tighi <monceftighiouart@gmail.com>
2022-10-13 13:24:58 +0100

    Querry pour comparer les frais de livraison

---
+++ SQL/yalidine.sql
@@ -0,0 +1,34 @@
+SELECT wilaya,COUNT(wilaya) AS "Livraison perdue" FROM ventes
+WHERE wilaya NOT IN (
+'Alger', 'Blida', 'Tizi ouzou', 'Jijel' ,'Souk Ahras'
+,'Boumerdes', 'Bouira' ,"M'sila", 'Guelma',
+'Tipaza', 'Medea', 'Tlemcen', 'Laghouat',
+'Ain defla', 'Bejaia', 'El Tarf',
+'Chlef', 'Setif', 'Khenchela',
+'Constantine', 'Batna',
+'Skikda', 'Biskra',
+'Oran', 'Tiaret','Mascara', 'Tebessa',
+'Annaba', 'Djelfa','Sidi Bel abbes', 'ghardaia'
+,'Aïn Témouchent', 'Bordj Bou Arreridj',
+'Tissemsilt','Mostaganem','Oum El Bouaghi','relizane',''
+)
+GROUP BY wilaya
+ORDER BY COUNT(wilaya) DESC;
+
+
+-------------------
+
+
+SELECT wilaya, AVG(frais_livraison) AS "Yalidine", AVG(new_frais_livraison) AS "Meslves" 
+FROM ventes
+WHERE new_frais_livraison>0 AND frais_livraison>0 
+GROUP BY wilaya;
+
+
+-------------------
+
+SELECT SUM(frais_livraison) AS "Yalidine", SUM(new_frais_livraison) AS "Meslves" 
+FROM ventes
+WHERE new_frais_livraison>0 AND frais_livraison>0 
+
+-------------------
